731758f3e1068ab280a32024947cb31eaf81a4ab
euichanhwang <pine6971@naver.com>
2023-04-16 18:17:47 +0900

    feat: 회원 저장 및 조회 기능을 구현한다

---
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,28 @@
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class Member {
+
+	private Long id;
+	private String username;
+	private int age;
+
+	public Member(String username, int age) {
+		this.username = username;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Member{" +
+			"id=" + id +
+			", username='" + username + '\'' +
+			", age=" + age +
+			'}';
+	}
+}
